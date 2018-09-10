f13d465ef161d622467c954b501b1b2fb484ddb0
alepmaros <alehstk@gmail.com>
2018-09-10 10:00:49 -0300

    More on the tree

--- src/tree.py
+++ src/tree.py
@@ -1,8 +1,11 @@
 class Node:
-    def __init__(self, val):
-        self.l = None
-        self.r = None
-        self.v = val
+    def __init__(self):
+        self.left  = None
+        self.right = None
+        self.value = None
+
+    def eval(self):
+        raise NotImplementedError
 
 class Tree:
     def __init__(self):
@@ -11,30 +14,50 @@ class Tree:
     def getRoot(self):
         return self.root
 
-    def add(self, val):
-        if(self.root == None):
-            self.root = Node(val)
-        else:
-            self._add(val, self.root)
+    def setRoot(self, root):
+        self.root = root
 
-    def _add(self, val, node):
-        if(val < node.v):
-            if(node.l != None):
-                self._add(val, node.l)
-            else:
-                node.l = Node(val)
-        else:
-            if(node.r != None):
-                self._add(val, node.r)
-            else:
-                node.r = Node(val)
+    def evalTree(self):
+        return self.root.eval()
 
-    def printTreeInOrder(self):
+    def __str__(self):
         if(self.root != None):
-            self._printTreeInOrder(self.root)
+            return self.root.__str__()
 
-    def _printTreeInOrder(self, node):
-        if(node != None):
-            self._printTreeInOrder(node.l)
-            print(str(node.v) + ' ')
-            self._printTreeInOrder(node.r)
+class Function(Node):
+    def __init__(self):
+        self.type = 'Function'
+
+class Sum(Function):
+    def __init__(self):
+        super().__init__()
+
+    def eval(self):
+        if (self.left == None or self.right == None):
+            raise Exception('Left or Right value not set for Sum')
+        
+        return self.left.eval() + self.right.eval()
+
+    def __str__(self):
+        return '(+ {} {})'.format(self.left.__str__(), self.right.__str__())
+
+class Terminal(Node):
+    def __init__(self, val):
+        super().__init__()
+        self.value = val
+
+    def eval(self):
+        return self.value
+
+    def __str__(self):
+        return str(self.value)
+
+a = Sum()
+a.left = Sum()
+a.left.left = Terminal(1)
+a.left.right = Terminal(2)
+a.right = Terminal(3)
+t = Tree()
+t.setRoot(a)
+print(t.evalTree())
+print(t)
