023560e16f7a59ed5291039304c511d833134aca
alepmaros <alehstk@gmail.com>
2018-09-18 14:41:55 -0300

    (maybe) fixed deepcopy and added sin

--- experiments.sh
+++ experiments.sh
@@ -8,20 +8,20 @@ echo "Synth 1"
 
 # Change in Population on Synth 1
 echo "Running with 50 Individuals"
-f1=$(python3 run.py -d synth1 -g 300 -k 5 -p 50 -r 20 -cp 0.85 -mp 0.10 -rp 0.05  --random-seed $SEED --timestamp $TIMESTAMP --test pop_test | tail -1)
+f1=$(python3 run.py -d synth1 -g 300 -k 5 -p 50 -r 30 -cp 0.85 -mp 0.10 -rp 0.05  --random-seed $SEED --timestamp $TIMESTAMP --test pop_test | tail -1)
 echo "Running with 100 Individuals"
-f2=$(python3 run.py -d synth1 -g 300 -k 5 -p 100 -r 20 -cp 0.85 -mp 0.10 -rp 0.05 --random-seed $SEED --timestamp $TIMESTAMP --test pop_test | tail -1)
+f2=$(python3 run.py -d synth1 -g 300 -k 5 -p 100 -r 30 -cp 0.85 -mp 0.10 -rp 0.05 --random-seed $SEED --timestamp $TIMESTAMP --test pop_test | tail -1)
 echo "Running with 500 Individuals"
-f3=$(python3 run.py -d synth1 -g 300 -k 5 -p 500 -r 20 -cp 0.85 -mp 0.10 -rp 0.05 --random-seed $SEED --timestamp $TIMESTAMP --test pop_test | tail -1)
+f3=$(python3 run.py -d synth1 -g 300 -k 5 -p 500 -r 30 -cp 0.85 -mp 0.10 -rp 0.05 --random-seed $SEED --timestamp $TIMESTAMP --test pop_test | tail -1)
 
 python3 plots/plot_population.py --scores $f1 $f2 $f3 --dataset synth1
 
-# # Operator Probabilities on Synth 1
-# echo "Running operator Probability 1"
-# f1=$(python3 run.py -d synth1 -g 300 -k 30 -p 100 -r 30 -cp 0.9 -mp 0.05 -rp 0.05 --random-seed $SEED --timestamp $TIMESTAMP --test operator_prob | tail -1)
-# echo "Running operator Probability 2"
-# f2=$(python3 run.py -d synth1 -g 300 -k 30 -p 100 -r 30 -cp 0.7 -mp 0.25 -rp 0.05 --random-seed $SEED --timestamp $TIMESTAMP --test operator_prob | tail -1)
-# echo "Running operator Probability 3"
-# f3=$(python3 run.py -d synth1 -g 300 -k 30 -p 100 -r 30 -cp 0.5 -mp 0.45 -rp 0.05 --random-seed $SEED --timestamp $TIMESTAMP --test operator_prob | tail -1)
+# Operator Probabilities on Synth 1
+echo "Running operator Probability 1"
+f1=$(python3 run.py -d synth1 -g 300 -k 30 -p 200 -r 30 -cp 0.9 -mp 0.05 -rp 0.05 --random-seed $SEED --timestamp $TIMESTAMP --test operator_prob | tail -1)
+echo "Running operator Probability 2"
+f2=$(python3 run.py -d synth1 -g 300 -k 30 -p 200 -r 30 -cp 0.7 -mp 0.25 -rp 0.05 --random-seed $SEED --timestamp $TIMESTAMP --test operator_prob | tail -1)
+echo "Running operator Probability 3"
+f3=$(python3 run.py -d synth1 -g 300 -k 30 -p 200 -r 30 -cp 0.5 -mp 0.45 -rp 0.05 --random-seed $SEED --timestamp $TIMESTAMP --test operator_prob | tail -1)
 
-# python3 plots/plot_operators_prob.py --scores $f1 $f2 $f3 --dataset synth1
+python3 plots/plot_operators_prob.py --scores $f1 $f2 $f3 --dataset synth1
--- src/tree.py
+++ src/tree.py
@@ -108,10 +108,8 @@ class Sum(Function):
         copy_object.parent       = None
         copy_object.left         = deepcopy(self.left)
         copy_object.right        = deepcopy(self.right)
-        if (copy_object.left != None):
-            copy_object.left.parent  = copy_object
-        if (copy_object.right != None):
-            copy_object.right.parent  = copy_object
+        copy_object.left.parent  = copy_object
+        copy_object.right.parent = copy_object
         return copy_object
 
 class Multiply(Function):
@@ -132,10 +130,8 @@ class Multiply(Function):
         copy_object.parent       = None
         copy_object.left         = deepcopy(self.left)
         copy_object.right        = deepcopy(self.right)
-        if (copy_object.left != None):
-            copy_object.left.parent  = copy_object
-        if (copy_object.right != None):
-            copy_object.right.parent  = copy_object
+        copy_object.left.parent  = copy_object
+        copy_object.right.parent  = copy_object
         return copy_object
 
 class Subtraction(Function):
@@ -156,10 +152,8 @@ class Subtraction(Function):
         copy_object.parent       = None
         copy_object.left         = deepcopy(self.left)
         copy_object.right        = deepcopy(self.right)
-        if (copy_object.left != None):
-            copy_object.left.parent  = copy_object
-        if (copy_object.right != None):
-            copy_object.right.parent  = copy_object
+        copy_object.left.parent  = copy_object
+        copy_object.right.parent = copy_object
         return copy_object
         
 class Division(Function):
@@ -184,10 +178,33 @@ class Division(Function):
         copy_object.parent       = None
         copy_object.left         = deepcopy(self.left)
         copy_object.right        = deepcopy(self.right)
-        if (copy_object.left != None):
-            copy_object.left.parent  = copy_object
-        if (copy_object.right != None):
-            copy_object.right.parent  = copy_object
+        copy_object.left.parent  = copy_object
+        copy_object.right.parent = copy_object
+        return copy_object
+
+class Sin(Function):
+    def __init__(self, parent, n_features, rng):
+        super().__init__(parent, n_features, rng)
+
+    def eval(self, X):
+        if (self.left == None or self.right == None):
+            raise Exception('Left or Right value not set for Division')
+        
+        with np.errstate(divide='ignore', invalid='ignore'):
+            result = np.sin(self.left.eval(X))
+            result[np.isinf(result)] = 1
+            result[np.isnan(result)] = 1
+            return result
+
+    def __str__(self):
+        return '(sin {})'.format(self.left.__str__(), self.right.__str__())
+
+    def __deepcopy__(self, memodict={}):
+        copy_object = Sin(None, self.n_features, self.rng)
+        copy_object.parent       = None
+        copy_object.left         = deepcopy(self.left)
+        copy_object.right        = None
+        copy_object.left.parent  = copy_object
         return copy_object
 
 #########################
@@ -218,12 +235,8 @@ class Value(Terminal):
         copy_object = Value(None, self.n_features, self.rng)
         copy_object.value        = self.value
         copy_object.parent       = None
-        copy_object.left         = deepcopy(self.left)
-        copy_object.right        = deepcopy(self.right)
-        if (copy_object.left != None):
-            copy_object.left.parent  = copy_object
-        if (copy_object.right != None):
-            copy_object.right.parent  = copy_object
+        copy_object.left         = None
+        copy_object.right        = None
         return copy_object
 
 class Variable(Terminal):
@@ -242,12 +255,8 @@ class Variable(Terminal):
         copy_object = Variable(None, self.n_features, self.rng)
         copy_object.value        = self.value
         copy_object.parent       = None
-        copy_object.left         = deepcopy(self.left)
-        copy_object.right        = deepcopy(self.right)
-        if (copy_object.left != None):
-            copy_object.left.parent  = copy_object
-        if (copy_object.right != None):
-            copy_object.right.parent  = copy_object
+        copy_object.left         = None
+        copy_object.right        = None
         return copy_object
 
 # X = np.array([[1, 2, 3], [3, 4, 5], [6, 7, 8]])
